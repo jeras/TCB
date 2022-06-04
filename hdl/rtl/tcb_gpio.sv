bbe9d781c57f2ef0c9f1ad2dd1f43590b701eff7
Iztok Jeras <iztok.jeras@gmail.com>
2022-06-04 07:51:08 +0200

    Cosmetic, fixing comment delimiter lengths

--- hdl/rtl/tcb_gpio.sv
+++ hdl/rtl/tcb_gpio.sv
@@ -18,7 +18,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 module tcb_gpio #(
   int unsigned GW = 32,   // GPIO width
