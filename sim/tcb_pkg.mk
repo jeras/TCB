ec6487401ce234881c78e531053cac2bcdaf4009
Iztok Jeras <iztok.jeras@gmail.com>
2023-09-17 22:12:54 +0200

    SIM: working toeard automatic regression tests

---
+++ sim/tcb_pkg.mk
@@ -0,0 +1,21 @@
+################################################################################
+# TCB package
+################################################################################
+
+# transfer size encoding
+TCB_LOGARITHMIC = 0
+TCB_LINEAR      = 1
+
+# data position mode
+TCB_REFERENCE = 0
+TCB_MEMORY    = 1
+
+# byte order
+TCB_DESCENDING = 0
+TCB_ASCENDING  = 1
+
+# channel configuration
+TCB_COMMON_HALF_DUPLEX = 0
+TCB_COMMON_FULL_DUPLEX = 3
+TCB_INDEPENDENT_WRITE  = 1
+TCB_INDEPENDENT_READ   = 2
