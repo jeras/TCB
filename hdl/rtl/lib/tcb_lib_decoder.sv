87e98a560fe22ce854a6cff26854c2f702ec726e
Iztok Jeras <iztok.jeras@gmail.com>
2025-04-07 02:34:26 +0200

    fixed parameter name

--- hdl/rtl/lib/tcb_lib_decoder.sv
+++ hdl/rtl/lib/tcb_lib_decoder.sv
@@ -21,16 +21,16 @@ module tcb_lib_decoder
 #(
   // TCB parameters (contains address width)
   parameter  tcb_par_phy_t PHY = TCB_PAR_PHY_DEF,
-  // interconnect parameters (manager port number and logarithm)
-  parameter  int unsigned MPN = 2,
-  localparam int unsigned MPL = $clog2(MPN),
+  // interconnect parameters (subordinate port number and logarithm)
+  parameter  int unsigned SPN = 2,
+  localparam int unsigned SPL = $clog2(SPN),
   // decoder address and mask array
-  parameter  logic [PHY.ABW-1:0] DAM [MPN-1:0] = '{default: 'x}
+  parameter  logic [PHY.ABW-1:0] DAM [SPN-1:0] = '{default: 'x}
 )(
   // TCB interfaces
   tcb_if.sub tcb,  // TCB subordinate port (manager device connects here)
   // select
-  output logic [MPL-1:0] sel
+  output logic [SPL-1:0] sel
 );
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -54,30 +54,30 @@ module tcb_lib_decoder
 ////////////////////////////////////////////////////////////////////////////////
 
 //  // match
-//  function [MPN-1:0] match (
+//  function [SPN-1:0] match (
 //    logic [PHY.ABW-1:0] val,           // input
-//    logic [PHY.ABW-1:0] mch [MPN-1:0]   // matching reference
+//    logic [PHY.ABW-1:0] mch [SPN-1:0]   // matching reference
 //  );
-//    for (int unsigned i=0; i<MPN; i++) begin
+//    for (int unsigned i=0; i<SPN; i++) begin
 //      assign match[i] = val ==? mch[i];
 //    end
 //  endfunction: match
 
   // match
-  logic mch [MPN-1:0];
+  logic mch [SPN-1:0];
 
   // match
   generate
-    for (genvar i=0; i<MPN; i++) begin
+    for (genvar i=0; i<SPN; i++) begin
       assign mch[i] = adr ==? DAM[i];
     end
   endgenerate
 
   // encode
-  function logic [MPL-1:0] encode (logic val [MPN-1:0]);
+  function logic [SPL-1:0] encode (logic val [SPN-1:0]);
     encode = 'x;  // optimization of undefined encodings
-    for (int i=MPN; i>=0; i--) begin
-      if (val[i])  encode = i[MPL-1:0];
+    for (int i=SPN; i>=0; i--) begin
+      if (val[i])  encode = i[SPL-1:0];
     end
   endfunction: encode
 
