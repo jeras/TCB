013ea0db1c77f4b16d9ddd2bce539d4cb9042c9b
Iztok Jeras <iztok.jeras@gmail.com>
2025-04-09 13:45:52 +0200

    unfinished updates to tcb_lib_riscv2memory

--- hdl/rtl/lib/tcb_lib_riscv2memory.sv
+++ hdl/rtl/lib/tcb_lib_riscv2memory.sv
@@ -75,10 +75,24 @@ module tcb_lib_riscv2memory
   logic [sub.PHY_BEN-1:0][sub.PHY.UNT-1:0] sub_req_wdt, sub_rsp_rdt;
   logic [man.PHY_BEN-1:0][man.PHY.UNT-1:0] man_req_wdt, man_rsp_rdt;
 
-  // write data multiplexer select
-  logic [$clog2(sub.PHY_BEN)-1:0] sel_req_wdt [man.PHY_BEN-1:0];
-  // read data multiplexer select
-  logic [$clog2(sub.PHY_BEN)-1:0] sel_rsp_rdt [man.PHY_BEN-1:0];
+  // request/response data byte enable
+  logic [sub.PHY_BEN-1:0] req_ben;
+  logic [sub.PHY_BEN-1:0] rsp_ben;
+  
+  // response unsigned
+  logic rsp_uns;
+
+  // write/read data multiplexer select
+  logic [$clog2(sub.PHY_BEN)-1:0] req_sel [man.PHY_BEN-1:0];
+  logic [$clog2(sub.PHY_BEN)-1:0] rsp_sel [man.PHY_BEN-1:0];
+
+  // address segment
+  int siz;
+  logic [$clog2(sub.PHY_BEN)-1:0] adr;
+
+  // address segment
+  assign siz = 2**sub.req.siz;
+  assign adr = sub.req.adr[$clog2(sub.PHY_BEN)-1:0];
 
   // write/read data packed array to/from vector
   assign sub_req_wdt = sub.req.wdt;
@@ -97,25 +111,20 @@ module tcb_lib_riscv2memory
 
     // byte mapping and signed/unsigned extension
     for (genvar i=0; i<man.PHY_BEN; i++) begin: byteenable
-      int siz = 2**sub.req.siz;
 
-      // multiplexer select signal
+      // multiplexer select signal (little/big endian)
       always_comb
       case (sub.req.ndn)
-        // little endian
-        1'b0: begin: little
-          sel_req_wdt[i] = (man.req.adr[$clog2(sub.PHY_BEN)-1:0]       + i) % sub.PHY_BEN;
-        end: little
-        // little endian
-        1'b1: begin: big
-          sel_req_wdt[i] = (man.req.adr[$clog2(sub.PHY_BEN)-1:0] + siz - i) % sub.PHY_BEN;
-        end: big
+        TCB_LITTLE:  req_sel[i] = (adr       + i) % sub.PHY_BEN;
+        TCB_BIG   :  req_sel[i] = (adr + siz - i) % sub.PHY_BEN;
       endcase
 
+      // byte enable
+      assign req_ben[i] = (i >= adr) & (i < adr+siz);
+
       // multiplexer
-      assign man.req.ben[i] = sub.req.ben[sel_req_wdt[i]];
-      assign man_req_wdt[i] = sub_req_wdt[sel_req_wdt[i]];
-      assign sub_rsp_rdt[i] = man_rsp_rdt[sel_rsp_rdt[i]];
+      assign man_req_wdt[i] = req_ben[        i ] ? sub_req_wdt[req_sel[i]] : 'x;
+      assign sub_rsp_rdt[i] = rsp_ben[rsp_sel[i]] ? man_rsp_rdt[rsp_sel[i]] : (rsp_uns ? '0 : {sub.PHY.UNT{man_rsp_rdt[(adr+siz-1) % sub.PHY_BEN][sub.PHY.UNT-1]}});
     end: byteenable
 
   endgenerate
@@ -123,9 +132,14 @@ module tcb_lib_riscv2memory
   // delay man_rsp_rdt
   // TODO: this now only works for DLY=1, generalize it
   always_ff @(posedge sub.clk)
-  sel_rsp_rdt <= sel_req_wdt;
+  begin
+    rsp_sel <=     req_sel;
+    rsp_ben <=     req_ben;
+    rsp_uns <= sub.req.uns;
+  end
 
   // write/read data packed array to/from vector
+  assign man.req.ben =     req_ben;
   assign man.req.wdt = man_req_wdt;
   assign man_rsp_rdt = man.rsp.rdt;
 
