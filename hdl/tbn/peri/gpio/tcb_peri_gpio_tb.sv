f23727ceaf07852ece04ede2fe0412615ab4aab1
Iztok Jeras <iztok.jeras@gmail.com>
2025-05-29 02:11:22 +0200

    updated GPIO testbench

---
+++ hdl/tbn/peri/gpio/tcb_peri_gpio_tb.sv
@@ -0,0 +1,203 @@
+////////////////////////////////////////////////////////////////////////////////
+// TCB GPIO testbench
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2022 Iztok Jeras
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+////////////////////////////////////////////////////////////////////////////////
+
+module tcb_peri_gpio_tb
+  import tcb_pkg::*;
+  import tcb_vip_blocking_pkg::*;
+#(
+  // TCB widths
+  int unsigned ADR = 32,
+  int unsigned DAT = 32
+);
+
+////////////////////////////////////////////////////////////////////////////////
+// local parameters
+////////////////////////////////////////////////////////////////////////////////
+
+  // GPIO width
+  localparam int unsigned GW = 32;
+
+  // handshake parameter
+  localparam tcb_hsk_t HSK = '{
+    DLY: 0
+  };
+
+  // bus parameter
+  localparam tcb_bus_t BUS = '{
+    ADR: TCB_BUS_DEF.ADR,
+    DAT: TCB_BUS_DEF.DAT,
+    FRM: TCB_BUS_DEF.FRM,
+    CHN: TCB_CHN_HALF_DUPLEX,
+    AMO: TCB_AMO_DISABLED,
+    PRF: TCB_PRF_DISABLED,
+    NXT: TCB_NXT_DISABLED,
+    MOD: TCB_MOD_LOG_SIZE,
+    ORD: TCB_ORD_DESCENDING,
+    NDN: TCB_NDN_BI_NDN
+  };
+
+  // physical interface parameter default
+  localparam tcb_pck_t PCK = '{
+    MIN: 0,
+    OFF: 0,
+    ALN: 0,
+    BND: 0
+  };
+
+  localparam tcb_vip_t VIP = '{
+    DRV: 1'b1
+  };
+
+//  typedef tcb_c #(HSK, BUS_SIZ, PCK)::req_t req_t;
+//  typedef tcb_c #(HSK, BUS_SIZ, PCK)::rsp_t rsp_t;
+
+  // local request/response types are copies of packaged defaults
+  typedef tcb_req_t req_t;
+  typedef tcb_rsp_t rsp_t;
+
+////////////////////////////////////////////////////////////////////////////////
+// local signals
+////////////////////////////////////////////////////////////////////////////////
+
+  // system signals (initialized)
+  logic clk = 1'b1;  // clock
+  logic rst = 1'b1;  // reset
+
+  string testname = "none";
+
+  // TCB interfaces
+  tcb_if #(tcb_hsk_t, HSK, tcb_bus_t, BUS, tcb_pck_t, PCK, req_t, rsp_t) tcb_man (.clk (clk), .rst (rst));
+
+  // parameterized class specialization (blocking API)
+  typedef tcb_vip_blocking_c #(tcb_hsk_t, HSK, tcb_bus_t, BUS, tcb_pck_t, PCK, req_t, rsp_t) tcb_man_s;
+
+  // TCB class objects
+  tcb_man_s obj_man = new(tcb_man, "MAN");
+
+  // response
+  logic [tcb_man.BUS_BEN-1:0][8-1:0] rdt;  // read data
+  tcb_rsp_sts_t                      sts;  // status response
+
+////////////////////////////////////////////////////////////////////////////////
+// data checking
+////////////////////////////////////////////////////////////////////////////////
+
+  logic [ 8-1:0] rdt8 ;  //  8-bit read data
+  logic [16-1:0] rdt16;  // 16-bit read data
+  logic [32-1:0] rdt32;  // 32-bit read data
+
+  // GPIO signals
+  logic [GW-1:0] gpio_o;
+  logic [GW-1:0] gpio_e;
+  logic [GW-1:0] gpio_i;
+
+////////////////////////////////////////////////////////////////////////////////
+// test sequence
+////////////////////////////////////////////////////////////////////////////////
+
+  // clock
+  always #(20ns/2) clk = ~clk;
+
+  // test sequence
+  initial
+  begin: test
+    // time dispaly formatting
+    $timeformat(-9, 3, "ns", 12);
+    // reset sequence
+    repeat (2) @(posedge clk);
+    rst <= 1'b0;
+    repeat (1) @(posedge clk);
+
+    // write configuration (output and enable registers)
+    $display("(%t) INFO: writing configuration begin.", $time);
+    obj_man.write32('h00, 32'h01234567, sts);  // write output register
+    obj_man.write32('h04, 32'h76543210, sts);  // write enable register
+    $display("(%t) INFO: writing configuration end.", $time);
+    repeat (1) @(posedge clk);
+
+    // check configuration (output and enable registers)
+    $display("(%t) INFO: reading/checking configuration begin.", $time);
+    obj_man.check32('h00, 32'h01234567, sts);  // write output register
+    obj_man.check32('h04, 32'h76543210, sts);  // write enable register
+    $display("(%t) INFO: reading/checking configuration end.", $time);
+    repeat (1) @(posedge clk);
+
+    // read/check GPIO input status
+    $display("(%t) INFO: reading/checking input begin.", $time);
+    obj_man.check32('h08, 32'hxxxxxxxx, '0);  // read input register
+
+    gpio_i <= GW'('h89abcdef);
+    repeat (2) @(posedge clk);
+    obj_man.check32('h08, 32'h89abcdef, '0);  // read input register
+
+    gpio_i <= GW'('hfedcba98);
+    repeat (2) @(posedge clk);
+    obj_man.check32('h08, 32'hfedcba98, '0);  // read input register
+    $display("(%t) INFO: reading/checking input end.", $time);
+
+    // TODO: add automatic testbench status report (SUCCESS/FAILURE)
+
+    // end simulation
+    repeat (4) @(posedge clk);
+    $finish();
+  end: test
+
+////////////////////////////////////////////////////////////////////////////////
+// VIP instances
+////////////////////////////////////////////////////////////////////////////////
+
+  tcb_vip_protocol_checker chk_man (
+    .tcb (tcb_man)
+  );
+
+////////////////////////////////////////////////////////////////////////////////
+// DUT instance
+////////////////////////////////////////////////////////////////////////////////
+
+  // TCB GPIO
+  tcb_peri_gpio #(
+    .GW      (GW),
+    // implementation details
+//    bit          CFG_MIN = 1'b0,  // minimalistic implementation
+    .CFG_CDC (2),
+    // implementation device (ASIC/FPGA vendor/device)
+    .CHIP    ("")
+  ) gpio (
+    // GPIO signals
+    .gpio_o  (gpio_o),
+    .gpio_e  (gpio_e),
+    .gpio_i  (gpio_i),
+    // TCB interface
+    .tcb     (tcb_man)
+  );
+
+////////////////////////////////////////////////////////////////////////////////
+// VCD/FST waveform trace
+////////////////////////////////////////////////////////////////////////////////
+
+  initial
+  begin
+`ifdef VERILATOR
+    $dumpfile("test.fst");
+`else
+    $dumpfile("test.vcd");
+`endif
+    $dumpvars;
+  end
+
+endmodule: tcb_peri_gpio_tb
--- sim/filelist.mk
+++ sim/filelist.mk
@@ -21,7 +21,7 @@ RTL+=${PATH_HDL}/rtl/lib/tcb_lib_demultiplexer.sv
 RTL+=${PATH_HDL}/rtl/lib/tcb_lib_logsize2byteena.sv
 RTL+=${PATH_HDL}/rtl/lib/tcb_lib_misaligned_memory_controller.sv
 # GPIO RTL
-#RTL+=${PATH_HDL}/rtl/peri/gpio/tcb_gpio.sv
+RTL+=${PATH_HDL}/rtl/peri/gpio/tcb_peri_gpio.sv
 # UART RTL
 #RTL+=${PATH_HDL}/rtl/peri/uart/tcb_uart_ser.sv
 #RTL+=${PATH_HDL}/rtl/peri/uart/tcb_uart_des.sv
@@ -51,7 +51,7 @@ TBN+=${PATH_HDL}/tbn/lib/tcb_lib_misaligned_memory_controller_tb.sv
 # SRAM model
 TBN+=${PATH_HDL}/tbn/peri/sram/sram_model.sv
 ## GPIO testbench
-#TBN+=${PATH_HDL}/tbn/peri/gpio/tcb_gpio_tb.sv
+TBN+=${PATH_HDL}/tbn/peri/gpio/tcb_peri_gpio_tb.sv
 ## UART testbench
 #TBN+=${PATH_HDL}/tbn/peri/uart/uart_model.sv
 #TBN+=${PATH_HDL}/tbn/peri/uart/uart_model_tb.sv
